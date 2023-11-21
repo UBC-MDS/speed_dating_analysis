f846324d3994e0266ed69995059650682917351e
Michelle Hunn <mihunn@student.ubc.ca>
2023-11-20 21:03:40 -0800

    Revert "function and tests for contour plot"
    This reverts commit 230df61d656c77d7208445b8f893cfd744bb34fb.

--- tests/testthat/helper_contour_plot.R
+++
@@ -1,5 +0,0 @@
-library(here)
-source(paste0(here(),'/R/contour_plot.R'))
-
-test_data <- data.frame(x = rnorm(1000), y = rnorm(1000))
-plot <- contour_plot_function(data = test_data, column1 = x, column2 = y, x_lab = 'X Variable', y_lab = 'Y Variable')

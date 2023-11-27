3177c5ebeec91f65be6b6ae6ef89756cd21d021c
Michelle Hunn <mihunn@student.ubc.ca>
2023-11-27 14:38:51 -0800

    added skeleton bash file

---
+++ bash_file.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+# create process for data cleaning, output cleaned file
+Rscript "script_name.R"
+
+# data analysis - t-test
+
+# data analzsis - pearson correlation
+
+# data vis - contour plot
+
+# data vis for - t-test
+
+# rendering the final report Rmd file

c175cd829e44ede23c2d40a7579a1137eb95bdd6
wenyunie <143786716+wenyunie@users.noreply.github.com>
2023-12-08 16:29:46 -0800

    Update t_test.R and corresponding testing functions

--- R/t_test.R
+++ R/t_test.R
@@ -11,14 +11,14 @@ library(tidyverse)
 #' @export
 #'
 #' @examples
-#' our_t_test(mtcars, am, bm, version='diff')
-our_t_test<-function(data,col1,col2,version='diff'){
-  if(version=='diff'){
+#' our_t_test(mtcars, am, bm, version = 'diff')
+our_t_test<-function(data, col1, col2, version = 'diff'){
+  if(version == 'diff'){
     data %>% 
-      mutate(diff={{col1}}-{{col2}}) %>%
-      t.test(x=.$diff,y=NULL,alternative='two.sided',mu=0,paired=FALSE)
+      mutate(diff = {{col1}} - {{col2}}) %>%
+      t.test(x = .$diff, y = NULL, alternative = 'two.sided', mu = 0, paired = FALSE)
   }else{
-    t.test(x=data %>% pull({{col1}}),
-           y=data %>% pull({{col2}}),alternative='two.sided',paired = TRUE,mu=0)
+    t.test(x = data %>% pull({{col1}}),
+           y = data %>% pull({{col2}}), alternative = 'two.sided', paired = TRUE, mu = 0)
   }
 }
